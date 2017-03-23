ef08e6e92a35552d4e78e9f805a6f78f434a071b
Ashley Yang <ashley.minghui@gmail.com>
2017-03-23 13:07:11 -0500

    Create and render first component

--- voting_app/app.js
+++ voting_app/app.js
@@ -1,1 +1,11 @@
+// React.createClass is the function to create components 
+// render is the key and it's value is an anonymous function that vanilla JS
+const ProductList = React.createClass({ render: function () {
+return (
+<div className='ui items'>
+        Hello, friend! I am a basic React component.
+      </div>
+); },
+});
 
+ReactDOM.render(<ProductList />, document.getElementById('content'));
